1de7eca1383cdfb85477a4d840402e65e743fa3f
JlikeJaime <jaimeperezgiron@gmail.com>
2026-04-15 20:35:21 +0200

    Añadido metodo SlotsSpellBook
    No hace nada, simplemente está creado

---
+++ Assets/Scripts/Player/SlotsSpellBook.cs
@@ -0,0 +1,43 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+public class SlotsSpellBook : MonoBehaviour
+{
+    private enum GameMode
+    {
+        allvsall,
+        knowledgerun,
+        friendly
+    }
+    //private List<MethodInfo> spells = new List<MethodInfo>();
+    //private List<FieldInfo> spellsInfo = new List<FieldInfo>();
+
+    void Start()
+    {
+        //foreach (var spell in typeof(SpellBase).GetMethods())
+        //{
+        //    spells.Add(spell);
+        //}
+        //foreach (var campo in typeof(SpellBase).GetFields())
+        //{
+        //    spellsInfo.Add(campo);
+        //}  
+    }
+
+    void Update()
+    {
+        
+    }
+
+    //private void spellWindow()
+    //{
+    //    GameObject Window = null;
+    //    //Window = gameObject.GetComponent<Canvas>();
+    //    if (Window != null)
+    //    {
+    //        Window.SetActive(true);
+    //    }
+    //}
+}
