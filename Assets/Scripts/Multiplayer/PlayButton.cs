03787a6bcb142c903fdba0ec4d9578acf7a2f5e0
JlikeJaime <jaimeperezgiron@gmail.com>
2026-04-28 21:13:10 +0200

    Cosas a futuro

--- Assets/Scripts/Multiplayer/PlayButton.cs
+++ Assets/Scripts/Multiplayer/PlayButton.cs
@@ -1,31 +1,44 @@
 using Unity.Services.Matchmaker;
 using UnityEngine;
+using System.Collections;
+using Unity.Netcode;
 
 public class PlayButton : MonoBehaviour
 {
     public async void Play()
     {
-        var ticket = await MatchmakerService.Instance.CreateTicketAsync(
-            new CreateTicketOptions { QueueName = "default" }
-        );
+        //var ticket = await MatchmakerService.Instance.CreateTicketAsync(
+        //    new CreateTicketOptions { QueueName = "default" }
+        //);
 
-        StartCoroutine(PollTicket(ticket.Id));
+        //StartCoroutine(PollTicket(ticket.Id));
     }
 
     private IEnumerator PollTicket(string ticketId)
     {
         while (true)
         {
-            var ticket = await MatchmakerService.Instance.GetTicketAsync(ticketId);
+            //var ticket = await MatchmakerService.Instance.GetTicketAsync(ticketId);
 
-            if (ticket.Status == "Matched")
-            {
-                Debug.Log("MATCH ENCONTRADO");
-                HandleMatch(ticket);
-                yield break;
-            }
+            //if (ticket.Status == "Matched")
+            //{
+            //    Debug.Log("MATCH ENCONTRADO");
+            //    //HandleMatch(ticket);
+            //    yield break;
+            //}
 
             yield return new WaitForSeconds(1);
         }
     }
+
+    //void HandleMatch(MatchmakingTicket ticket)
+    //{
+    //    bool isHost = ticket.Players[0].Id == AuthenticationService.Instance.PlayerId;
+
+    //    if (isHost)
+    //        StartHostFlow();
+    //    else
+    //        StartClientFlow(ticket);
+    //}
+
 }
--- Assets/Scripts/Multiplayer/StartClientFlow.cs
+++ Assets/Scripts/Multiplayer/StartClientFlow.cs
@@ -1,16 +1,30 @@
-using UnityEngine;
+//using Unity.Services.Lobbies;
+//using Unity.Services.Relay;
+//using Unity.Netcode;
+//using Unity.Netcode.Transports.UTP;
 
-public class StartClientFlow : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+//public async void StartClientFlow(MatchmakingTicket ticket)
+//{
+//    // 1. Unirse al Lobby (ejemplo simplificado)
+//    var lobby = await LobbyService.Instance.JoinLobbyByIdAsync(ticket.LobbyId);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+//    // 2. Leer joinCode
+//    string joinCode = lobby.Data["joinCode"].Value;
+
+//    // 3. Conectarse a Relay
+//    var allocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+
+//    var transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+
+//    transport.SetRelayServerData(
+//        allocation.RelayServer.IpV4,
+//        (ushort)allocation.RelayServer.Port,
+//        allocation.AllocationIdBytes,
+//        allocation.Key,
+//        allocation.ConnectionData,
+//        allocation.HostConnectionData
+//    );
+
+//    // 4. Start Client
+//    NetworkManager.Singleton.StartClient();
+//}
