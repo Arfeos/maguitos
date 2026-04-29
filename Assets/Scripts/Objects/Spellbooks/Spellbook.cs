41c097bd2f1db63eda3d69371c7b5620fca3e3bf
Sergio Martínez Molina <100209380+SergiMaMo@users.noreply.github.com>
2026-04-29 17:43:28 +0200

    spellbook created
    spellbook created

---
+++ Assets/Scripts/Objects/Spellbooks/Spellbook.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Spellbook : MonoBehaviour
+{
+    [SerializeField] private SpellBase Spell;
+    private void Awake()
+    {
+        
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
