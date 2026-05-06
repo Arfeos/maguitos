bcb35fdee0030537c216076c427ec04d60d5796c
Arfeos <83002671+Arfeos@users.noreply.github.com>
2026-05-06 18:49:15 +0200

    a

--- Assets/ScriptableObjects/SpellScriptables/SpellBaseScriptable.cs
+++ Assets/ScriptableObjects/SpellScriptables/SpellBaseScriptable.cs
@@ -29,6 +29,7 @@ public class SpellBaseScriptable : ScriptableObject
     public GameObject? spawnPrefab;
     public GameObject? producedParticle;
     public GameObject? hitParticle;
+    public GameObject? trailParticle;
 
     [Header("Spell sound")]
     public AudioSource? spawnSound;
