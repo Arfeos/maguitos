8317c68f16cfa70e93312070453010b8aaf3ecbc
Sergio Martínez Molina <100209380+SergiMaMo@users.noreply.github.com>
2026-05-09 21:32:50 +0200

    Tutorial terminado, a falta de reunirnos

--- Assets/Scripts/Services/Score/ScoreService.cs
+++ Assets/Scripts/Services/Score/ScoreService.cs
@@ -1,6 +1,49 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ScoreService : IScoreService
 {
-    
+    public Dictionary<string, int> PlayerList = new Dictionary<string, int>();
+
+    public void addPoints(string player, int points)
+    {
+        if (PlayerList.TryGetValue(player, out int playerPoints))
+        {
+            playerPoints += points;
+            PlayerList[player] = playerPoints;
+        }
+        else
+        {
+            PlayerList.Add(player, points);
+        }
+    }
+
+    public int GetPoints(string player)
+    {
+        if (PlayerList.TryGetValue(player, out int playerPoints)) return playerPoints;
+        else
+        {
+            PlayerList.Add(player, 0);
+            return 0;
+        }
+            
+    }
+
+    public void removePoints(string player, int points)
+    {
+        if (PlayerList.TryGetValue(player, out int playerPoints))
+        {
+            playerPoints -= points;
+            PlayerList[player] = playerPoints;
+        }
+        else
+        {
+            PlayerList.Add(player, -points);
+        }
+    }
+
+    public void resetScore()
+    {
+        PlayerList = new Dictionary<string, int>();
+    }
 }
